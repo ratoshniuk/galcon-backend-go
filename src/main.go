10874eb73b36db8ad5280d85cbb5058b8a2ca2c8
ratoshniuk <mratoshniuk@gmail.com>
2018-09-07 16:54:31 +0300

    added branches regex and fetching sys env

--- src/main.go
+++ src/main.go
@@ -2,12 +2,19 @@ package main
 
 import (
     "flag"
+    "fmt"
     "github/com/galcon-backend-go/ws"
+    "os/exec"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
 
 func main() {
+
+    res := exec.Command("echo $HEROKU_API_SECRET")
+
+    fmt.Printf("%+v", res)
+
     ws.StartServer(*addr)
     ws.StartClient(*addr)
 }
