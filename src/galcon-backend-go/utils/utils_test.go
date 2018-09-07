456d5608bd38204aa279384956b9c1d9a367d70c
ratoshniuk <mratoshniuk@gmail.com>
2018-09-07 17:52:00 +0300

    sss

--- src/galcon-backend-go/utils/utils_test.go
+++ src/galcon-backend-go/utils/utils_test.go
@@ -2,16 +2,19 @@ package utils
 
 import (
     "fmt"
+    "log"
     "os/exec"
     "testing"
 )
 
 func TestUtils(t *testing.T) {
-    res, err := exec.Command("echo $FOO").Output()
+    cmd := exec.Command("echo $GOPATH")
+    stdout, err := cmd.StdoutPipe()
     if err != nil {
-
+        log.Fatal(err)
     }
-
-    fmt.Printf("ssss")
-    fmt.Printf(string(res))
+    if err := cmd.Start(); err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("%+v", stdout)
 }
