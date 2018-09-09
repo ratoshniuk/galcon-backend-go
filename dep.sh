95f9af2221f6391251ed111940df01df08e331c1
ratoshniuk <mratoshniuk@gmail.com>
2018-09-09 20:26:05 +0300

    some logs added for downloading dependencies

--- dep.sh
+++ dep.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
 
-go get github.com/gorilla/websocket
+echo "installing all deps.."
+
+get_dep () {
+echo "installing $1"
+go get $2 $1
+}
+
+get_dep github.com/gorilla/websocket
+get_dep github.com/gorilla/mux
+get_dep github.com/jinzhu/gorm
+
+
+echo "...done"
